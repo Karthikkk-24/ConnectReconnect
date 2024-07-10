bf640decb0441b58b5055a977a200e3d8c8e6a3a
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-07-10 16:15:10 +0530

    Changes to Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Explore from './components/Explore';
-import Home from './components/Home';
 import Login from './components/Login';
 import Main from './components/Main';
 import Register from './components/Register';
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary': '#BFF500',
+                'secondary': '#161616',
+            },
+        },
     },
     plugins: [],
 }
