1e91b877db2ce2a2eac61e4b5584f8425646a198
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-07-10 08:37:27 +0530

    Added other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import Explore from './components/Explore';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Updates from './components/Updates';
 function App() {
-  return (
-    <>
-      <h1 className="text-green-300 underline">
-      </h1>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/updates" element={<Updates />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Updates.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Updates() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
