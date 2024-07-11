73910b213a72314b5fd035e2760a45e3db4c7cd4
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-07-12 00:09:33 +0530

    Changes to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,27 @@
-
-
 export default function Sidebar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="w-96 bg-secondary h-screen flex flex-col items-start justify-start gap-4 py-6 px-3">
+            <a href="">
+                Demo
+            </a>
+            <a href="">
+                Demo
+            </a>
+            <a href="">
+                Demo
+            </a>
+            <a href="">
+                Demo
+            </a>
+            <a href="">
+                Demo
+            </a>
+            <a href="">
+                Demo
+            </a>
+            <a href="">
+                Demo
+            </a>
+        </div>
+    );
 }
